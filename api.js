32a8a03faf2523c7ba0dc3062dfc12e71286c475
Mahdy Nesar Mahy <mahdynesarmahy8466@gmail.com>
2025-09-26 06:02:11 -0400

    chore(api): add server listener and comments

--- api.js
+++ api.js
@@ -12,3 +12,6 @@ app.post('/users', (req, res) => {
 app.get('/products', (req, res) => {
   res.send("List of products");
 });
+app.listen(3000, () => {
+  console.log("API server running on port 3000");
+});
