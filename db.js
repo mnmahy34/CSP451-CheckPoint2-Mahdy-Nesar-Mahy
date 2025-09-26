3ebbbf3dc86b92d25990d22bab58c0f7812fe046
Mahdy Nesar Mahy <mahdynesarmahy8466@gmail.com>
2025-09-26 05:58:25 -0400

    feat(db): add basic database connection module

---
+++ db.js
@@ -0,0 +1,15 @@
+// Database connection simulation
+const db = {
+  host: "localhost",
+  user: "admin",
+  password: "password",
+  database: "team_project"
+};
+
+function connect() {
+  console.log("Connecting to database at " + db.host);
+  // Fake connection success
+  return true;
+}
+
+module.exports = { connect };
